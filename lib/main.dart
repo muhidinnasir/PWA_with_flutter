7cfc0da95236d92ead890873c36e0b5f3f0b98a5
muhidinnasir <muhidinnasir48@gmail.com>
2024-05-31 20:33:01 +0300

    2nd pwa for ashe

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
@@ -18,10 +20,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Branmetron',
+      title: 'Radian',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueGrey),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: ''),
@@ -44,7 +46,7 @@ class _MyHomePageState extends State<MyHomePage> {
   bool hasInternet = false;
 
   initializSplash() async {
-    await Future.delayed(const Duration(seconds: 6));
+    await Future.delayed(const Duration(seconds: 3));
     FlutterNativeSplash.remove();
   }
 
@@ -80,47 +82,123 @@ class _MyHomePageState extends State<MyHomePage> {
       },
       child: SafeArea(
         child: Scaffold(
-            body: hasInternet
-                ? Stack(
-                    children: [
-                      InAppWebView(
-                        initialUrlRequest: URLRequest(
-                            url: Uri.parse(
-                                "https://brainmetron.kibrewosen.com")),
-                        onWebViewCreated: (InAppWebViewController controller) {
-                          inAppWebViewController = controller;
-                        },
-                        onProgressChanged:
-                            (InAppWebViewController controller, int progress) {
-                          setState(() {
-                            _progress = progress / 100;
-                          });
-                        },
+          body: hasInternet
+              ? Stack(
+                  children: [
+                    InAppWebView(
+                      initialUrlRequest: URLRequest(
+                        url: Uri.parse("http://49.13.153.18:1011/"),
                       ),
-                      _progress < 1
-                          ? LinearProgressIndicator(
-                              value: _progress,
-                            )
-                          : const SizedBox()
-                    ],
-                  )
-                : const Center(
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(
-                          Icons.error,
-                          color: Colors.red,
-                        ),
-                        Padding(
-                          padding: EdgeInsets.only(left: 8.0),
-                          child: Text('No Internet Connection'),
-                        ),
-                      ],
+                      onWebViewCreated: (InAppWebViewController controller) {
+                        inAppWebViewController = controller;
+                      },
+                      // shouldOverrideUrlLoading:
+                      //     (controller, navigationAction) async {
+                      //   var url = navigationAction.request.url;
+                      //   if (!url.toString().contains('nehabi.ashewa.com')) {
+                      //     // Cancel the navigation
+                      //     return NavigationActionPolicy.CANCEL;
+                      //   } else {
+                      //     // Allow the navigation to proceed
+                      //     return NavigationActionPolicy.ALLOW;
+                      //   }
+                      // },
+                      // onLoadStart: (controller, url) {
+                      //   if (!url.toString().contains('nehabi.ashewa.com')) {
+                      //     // Cancel the navigation
+                      //     controller.stopLoading();
+                      //     // Open the link using your custom method
+                      //     _launchUrl(url.toString());
+                      //   }
+                      // },
+                      onProgressChanged:
+                          (InAppWebViewController controller, int progress) {
+                        setState(() {
+                          _progress = progress / 100;
+                        });
+                      },
                     ),
-                  )),
+                    _progress < 1
+                        ? LinearProgressIndicator(
+                            value: _progress,
+                            color: Colors.green,
+                          )
+                        : const SizedBox(),
+                    // _progress < 1
+                    //     ? Align(
+                    //         child: SizedBox(
+                    //           height: MediaQuery.of(context).size.height,
+                    //           width: MediaQuery.of(context).size.width,
+                    //           child: const Center(
+                    //             child: Column(
+                    //               mainAxisAlignment: MainAxisAlignment.center,
+                    //               children: [
+                    //                 CircularProgressIndicator(
+                    //                   color: Colors.green,
+                    //                 ),
+                    //                 Row(
+                    //                   crossAxisAlignment:
+                    //                       CrossAxisAlignment.center,
+                    //                   mainAxisAlignment:
+                    //                       MainAxisAlignment.center,
+                    //                   children: [
+                    //                     Padding(
+                    //                       padding: EdgeInsets.only(left: 8.0),
+                    //                       child: Text(
+                    //                           'Page is loading, Please wait...'),
+                    //                     ),
+                    //                   ],
+                    //                 ),
+                    //               ],
+                    //             ),
+                    //           ),
+                    //         ),
+                    //       )
+                    //     : const SizedBox.shrink(),
+                  ],
+                )
+              : const Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      CircularProgressIndicator(color: Colors.green),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.error,
+                            color: Colors.red,
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(left: 8.0),
+                            child: Text('No Internet Connection'),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+        ),
       ),
     );
+  }
+
+  // Open social media
+  void _launchUrl(url) async {
+    try {
+      if (await canLaunchUrl(Uri.parse(url))) {
+        await launchUrl(Uri.parse(url), mode: LaunchMode.externalApplication)
+            .then((value) {});
+      } else {
+        throw 'Could not launch $url';
+      }
+    } on PlatformException catch (e) {
+      if (e.code == 'LAUNCH_FAILED_NO_APP') {
+        return;
+      }
+    } catch (e) {
+      return;
+    }
   }
 }
